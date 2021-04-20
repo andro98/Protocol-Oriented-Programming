140de50e42ba3c238ea32c0ceeb95f09292f3c41
Andrew Maher <andrewaacodo12@gmail.com>
2021-04-20 20:56:25 +0200

    Protocol Inheritance

--- Contents.swift
+++ Contents.swift
@@ -84,3 +84,60 @@ let shapes: [Shape] = [
 for shape in shapes{
     print(shape.area)
 }
+
+
+// MARK:- Inheritance
+
+protocol TaggedPersistable: Taggable, CustomStringConvertible{
+    init(tag: String, contentsOf url: URL) throws
+    func persist(to url: URL) throws
+}
+
+protocol TaggedEncodable: Taggable{
+    var base64: String { get }
+}
+
+struct MyData: Taggable {
+    var tag: String
+    var data: Data
+    
+    static var counter: Int = 0
+    
+    mutating func update(data: Data) -> Bool {
+        self.data = data
+        return true
+    }
+    
+    static func incrementCounter() {
+        counter += counter
+    }
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+extension MyData: TaggedPersistable{
+    init(tag: String, contentsOf url: URL) throws {
+        let data = try Data.init(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+    
+    func persist(to url: URL) throws {
+        try self.data.write(to: url)
+    }
+}
+
+extension MyData: TaggedEncodable{
+    var base64: String{
+        return self.data.base64EncodedString()
+    }
+    
+}
+
+extension MyData: CustomStringConvertible{
+    var description: String{
+        return "MyData (\(tag))"
+    }
+}
