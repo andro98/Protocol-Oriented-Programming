2aa48a51142f6f94ef4d8fa8d478cbfc578dbab0
Andrew Maher <andrewaacodo12@gmail.com>
2021-04-20 20:28:01 +0200

    Define protocol and Confirm to it

---
+++ Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+// MARK:- Defining Protocol and Confirm to it
+// Protocl Namin
+// Start with a capital letter
+// Noun + suffix to describe a capability
+
+protocol Taggable{
+    var tag: String { get set }
+    var data: Data { get }
+    static var counter: Int { get }
+    
+    mutating func update(data: Data) -> Bool
+    static func incrementCounter()
+    
+    init(tag: String, data: Data)
+}
+
+
+struct TaggedData: Taggable{
+    var tag: String
+    
+    var data: Data
+    
+    static var counter: Int = 0
+    
+    mutating func update(data: Data) -> Bool {
+        self.data = data
+        return true
+    }
+    
+    static func incrementCounter() {
+        counter += 1
+    }
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
